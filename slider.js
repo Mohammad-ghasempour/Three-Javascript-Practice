9c2f3e38c067190a6075639cf73026549954e1bf
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-16 14:17:08 +0100

    back button improved but sometime a click doesn't work correctly!

--- slider.js
+++ slider.js
@@ -1,17 +1,34 @@
-const left = document.querySelector(".left")
-const right = document.querySelector(".right")
-const slider = document.querySelector(".slider")
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
 const allImages = document.querySelectorAll(".images");
 
 let sliderNumber = 1;
-right.addEventListener("click" , ()=>{
-   if (sliderNumber < allImages.length ){
-       slider.style.transform = `translateX(-${sliderNumber * 600}px)`;
-       sliderNumber ++;
-   } else{
-    slider.style.transform = `translateX(0px)`;
-    sliderNumber=1;
-   }
+const imagesLength = allImages.length;
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${sliderNumber * 600}px)`;
+  sliderNumber++;
+};
+
+const firstSlide = () => {
+  slider.style.transform = `translateX(0px)`;
+  sliderNumber = 1;
+};
+right.addEventListener("click", () => {
+  sliderNumber < imagesLength ? nextSlide() : firstSlide();
+});
+
+
+const prevSlide = () => {
+    //slider.style.transform = `translateX(-${sliderNumber * 600}px)`;
+    sliderNumber--;
+    slider.style.transform = `translateX(-${(sliderNumber * 600) - 600}px)`;
+    //sliderNumber--;
    
-  
+  };
+
+
+
+left.addEventListener("click" , ()=>{
+    prevSlide()
 })
