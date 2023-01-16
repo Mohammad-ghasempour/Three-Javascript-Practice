c998b330bce41048ad100c84cdb8cb530edfdee5
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-16 12:40:17 +0100

    right click button to change the image has been made

--- slider.js
+++ slider.js
@@ -1,1 +1,17 @@
-console.log("it's work!");
+const left = document.querySelector(".left")
+const right = document.querySelector(".right")
+const slider = document.querySelector(".slider")
+const allImages = document.querySelectorAll(".images");
+
+let sliderNumber = 1;
+right.addEventListener("click" , ()=>{
+   if (sliderNumber < allImages.length ){
+       slider.style.transform = `translateX(-${sliderNumber * 600}px)`;
+       sliderNumber ++;
+   } else{
+    slider.style.transform = `translateX(0px)`;
+    sliderNumber=1;
+   }
+   
+  
+})
