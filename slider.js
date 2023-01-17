8d60c17b866a82a518b074113c14ddce6afcc0e5
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-17 10:20:44 +0100

    one solution for creating buttons under photo box with changing the color during navigation

--- slider.js
+++ slider.js
@@ -2,33 +2,60 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const slider = document.querySelector(".slider");
 const allImages = document.querySelectorAll(".images");
+const bottom = document.querySelector(".bottom");
 
 let sliderNumber = 1;
 const imagesLength = allImages.length;
+
+
 const nextSlide = () => {
   slider.style.transform = `translateX(-${sliderNumber * 600}px)`;
+  
+  buttons[sliderNumber-1].style.backgroundColor = '';
+  buttons[sliderNumber].style.backgroundColor = 'white';
   sliderNumber++;
 };
 
 const firstSlide = () => {
   slider.style.transform = `translateX(0px)`;
+  buttons[sliderNumber-1].style.backgroundColor = '';
+  
   sliderNumber = 1;
+  buttons[sliderNumber-1].style.backgroundColor = 'white';
 };
+
+const lastSlide = () => {
+  slider.style.transform = `translateX(-${(imagesLength - 1) * 600}px)`;
+  
+  buttons[sliderNumber-1].style.backgroundColor = '';
+  buttons[imagesLength-1].style.backgroundColor = 'white';
+  sliderNumber = imagesLength;
+};
+
+const prevSlide = () => {
+  slider.style.transform = `translateX(-${(sliderNumber - 2) * 600}px)`;
+  buttons[sliderNumber-1].style.backgroundColor = '';
+  buttons[sliderNumber-2].style.backgroundColor = 'white';
+   sliderNumber--;
+};
+
+
+
 right.addEventListener("click", () => {
   sliderNumber < imagesLength ? nextSlide() : firstSlide();
 });
-
-
-const prevSlide = () => {
-    //slider.style.transform = `translateX(-${sliderNumber * 600}px)`;
-    sliderNumber--;
-    slider.style.transform = `translateX(-${(sliderNumber * 600) - 600}px)`;
-    //sliderNumber--;
-   
-  };
+left.addEventListener("click", () => {
+  sliderNumber > 1 ? prevSlide() : lastSlide();
+});
 
 
 
-left.addEventListener("click" , ()=>{
-    prevSlide()
-})
+for (let i=0 ; i < imagesLength; i++) {
+
+  const div = document.createElement("div");
+  div.className = "button";
+  bottom.appendChild(div);
+}
+
+const buttons = document.querySelectorAll('.button');
+buttons[0].style.backgroundColor = 'white';
