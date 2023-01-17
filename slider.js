fe7ec023cdd785ef8168e9bce2bd32f10cc409ea
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-17 11:33:36 +0100

    a reset background color function has been made and the project is finished.

--- slider.js
+++ slider.js
@@ -7,39 +7,40 @@ const bottom = document.querySelector(".bottom");
 let sliderNumber = 1;
 const imagesLength = allImages.length;
 
-
+const resetBG = () => {
+  buttons.forEach((button) => {
+    button.style.backgroundColor = "transparent";
+  });
+};
 const nextSlide = () => {
   slider.style.transform = `translateX(-${sliderNumber * 600}px)`;
-  
-  buttons[sliderNumber-1].style.backgroundColor = '';
-  buttons[sliderNumber].style.backgroundColor = 'white';
+
+  resetBG();
+  buttons[sliderNumber].style.backgroundColor = "white";
   sliderNumber++;
 };
 
 const firstSlide = () => {
   slider.style.transform = `translateX(0px)`;
-  buttons[sliderNumber-1].style.backgroundColor = '';
-  
+  resetBG();
   sliderNumber = 1;
-  buttons[sliderNumber-1].style.backgroundColor = 'white';
+  buttons[sliderNumber - 1].style.backgroundColor = "white";
 };
 
 const lastSlide = () => {
   slider.style.transform = `translateX(-${(imagesLength - 1) * 600}px)`;
-  
-  buttons[sliderNumber-1].style.backgroundColor = '';
-  buttons[imagesLength-1].style.backgroundColor = 'white';
+
+  resetBG();
+  buttons[imagesLength - 1].style.backgroundColor = "white";
   sliderNumber = imagesLength;
 };
 
 const prevSlide = () => {
   slider.style.transform = `translateX(-${(sliderNumber - 2) * 600}px)`;
-  buttons[sliderNumber-1].style.backgroundColor = '';
-  buttons[sliderNumber-2].style.backgroundColor = 'white';
-   sliderNumber--;
+  resetBG();
+  buttons[sliderNumber - 2].style.backgroundColor = "white";
+  sliderNumber--;
 };
-
-
 
 right.addEventListener("click", () => {
   sliderNumber < imagesLength ? nextSlide() : firstSlide();
@@ -48,14 +49,20 @@ left.addEventListener("click", () => {
   sliderNumber > 1 ? prevSlide() : lastSlide();
 });
 
-
-
-for (let i=0 ; i < imagesLength; i++) {
-
+for (let i = 0; i < imagesLength; i++) {
   const div = document.createElement("div");
   div.className = "button";
   bottom.appendChild(div);
 }
 
-const buttons = document.querySelectorAll('.button');
-buttons[0].style.backgroundColor = 'white';
+const buttons = document.querySelectorAll(".button");
+buttons[0].style.backgroundColor = "white";
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    slider.style.transform = `translateX(-${index * 600}px)`;
+
+    resetBG();
+    button.style.backgroundColor = "white";
+  });
+});
