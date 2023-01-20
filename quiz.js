f17dea7edc3fbdcf95262cdfc43dc0cc9811afd1
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-20 15:49:58 +0100

    create data as questions and its answer

---
+++ quiz.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+        id: 1,
+        question: "What is the capital of Norway?",
+        answers: [
+            {answer: "Oslo" , isCorrect: true},
+            {answer: "Stavanger" , isCorrect: false},
+            {answer: "Bergen" , isCorrect: false},
+            {answer: "Trondheim" , isCorrect: false},
+        ]
+
+    },{
+        id: 2,
+        question: "Which currency is the Norway currency?",
+        answers: [
+            {answer: "Euro" , isCorrect: false},
+            {answer: "Dollar" , isCorrect: false},
+            {answer: "Pound" , isCorrect: false},
+            {answer: "Krone" , isCorrect: true},
+        ]
+
+    },{
+        id: 3,
+        question: "What is the population of Norway?",
+        answers: [
+            {answer: "1,422,687 (about the same as Maine)" , isCorrect: false},
+            {answer: "5,434,319 (almost that of Alabama)" , isCorrect: true},
+            {answer: "807,413 (similar to South Dakota)" , isCorrect: false},
+            {answer: "37,961,664 (approximately that of California)" , isCorrect: false},
+        ]
+
+    }
+]
