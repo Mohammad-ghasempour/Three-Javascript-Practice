94cc8e940991996b4c36a533562245cfd07fca0e
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-01-21 15:44:51 +0100

    load qestion and answer from our data file to dom

--- quiz.js
+++ quiz.js
@@ -31,3 +31,28 @@ const data = [
 
     }
 ]
+
+
+const gameScreen = document.querySelector(".game");
+const resultScreen = document.querySelector(".result");
+const question = document.querySelector(".question");
+const answersContainer = document.querySelector(".answers");
+const submit = document.querySelector(".submit");
+const play = document.querySelector(".play");
+
+let qIndex = 0;
+let correctCount= 0;
+let wrongCount= 0;
+let total= 0;
+let selectedAnswer;
+
+const showQuestion = (qNumber)=>{
+question.textContent= data[qNumber].question;
+answersContainer.innerHTML= data[qNumber].answers.map((item , index)=> ` <div class="answer">
+<input type="radio" id=${index} name="answer" value=${item.isCorrect} />
+<label for="answer1">${item.answer}</label>
+</div>`).join("");
+}
+
+showQuestion(qIndex);
+
